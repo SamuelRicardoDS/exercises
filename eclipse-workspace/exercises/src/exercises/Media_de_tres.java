67057bd47789446e5a01720286bedf8e5cba0961
SamuelRicardoDS <samuelricardods@gmail.com>
2022-06-13 12:39:56 -0300

    media

---
+++ eclipse-workspace/exercises/src/exercises/Media_de_tres.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class Media_de_tres {
+	public static void main(String[] args) {
+	  Double num_um = 1.58;
+      Double num_dois = 2.07;
+      Double num_tres = 0.55;
+      
+      Double media = (num_um + num_dois + num_tres)/3;
+      
+      System.out.println("a média é:" + media);
+      
+	}
+}
