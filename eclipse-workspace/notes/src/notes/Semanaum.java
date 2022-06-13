cd44b04faa58ad2e804939f6148a6f29f19d9cec
SamuelRicardoDS <samuelricardods@gmail.com>
2022-06-13 12:32:08 -0300

    test

---
+++ eclipse-workspace/notes/src/notes/Semanaum.java
@@ -0,0 +1,14 @@
+package notes;
+
+public class Semanaum {
+	//programa é organizado por objetos, um interage com o outro por meio de mensagens
+	//objeto é um dado que queremos armazenar
+	//aumento da produtividade, reúso oureaproveitamento de código, uso de componentes, flexibilização de software
+	//escalabilidade facilidade de manutenção
+	//pilares da orientação a objetos: abstração, encapsulamento, herança, polimorfismo
+	//classe não é um objeto, é um modelo, uma receita de como criar um objeto
+	public static void main(String[] args) {
+		System.out.println("o mundo é orientado a objetos");
+	}
+
+}
