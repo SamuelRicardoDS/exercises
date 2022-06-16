4d2fc998ba989fb437ebf0f922774acc7e97f912
SamuelRicardoDS <samuelricardods@gmail.com>
2022-06-16 16:12:28 -0300

    exercícios da semana um

---
+++ eclipse-workspace/exercises/src/exercises/Operacoes_matematicas.java
@@ -0,0 +1,25 @@
+package exercises;
+import java.util.Scanner;
+
+public class Operacoes_matematicas {
+	public static void main(String[] args) {
+		
+		String operacao;
+		int numum;
+		int numdois;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Entre com a operação a ser realizada (+, - ou *):\n");
+		operacao = ler.next();
+		System.out.print("Entre com o primeiro número:\n");
+		numum = ler.nextInt();
+		System.out.print("Entre com o segundo número:\n");
+		numdois = ler.nextInt();
+		
+		
+		
+		
+	}
+
+}
